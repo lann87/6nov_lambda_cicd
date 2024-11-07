439c5654704c7da4226dd95cbe1ae114f1fc0029
Alan Peh <alanpeh87@gmail.com>
2024-11-07 15:25:28 +0800

    troubleshooting checkov vuln 2.0

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "ap-lambda-fn" {
   #checkov:skip=CKV_AWS_117:Ensure that AWS Lambda function is configured inside a VPC
-  function_name    = "ap-lambda-function-6nov"
-  runtime          = "python3.10"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
+  function_name = "ap-lambda-function-6nov"
+  runtime       = "python3.10"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
 
-  code_signing_config_arn = aws_lambda_code_signing_config.ap-lambda
+  code_signing_config_arn        = aws_lambda_code_signing_config.ap-lambda
   reserved_concurrent_executions = 50
   dead_letter_config {
     target_arn = "test"
